6415209fb22e7120d89dfe9a554b9420ffb591ff
Maximilian Narr <maxe.nr@live.de>
2018-04-10 15:22:40 +0200

    Added preliminary DB Schema

---
+++ Doc/Schema.sql
@@ -0,0 +1,65 @@
+﻿DROP TABLE IF EXISTS wishlist_products;
+DROP TABLE IF EXISTS orders_products;
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS product_images;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS product_categories;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS addresses;
+
+CREATE TABLE addresses (
+    id          	CHAR(36)    PRIMARY KEY
+    ,name	        TEXT	    NOT NULL
+    ,street     	TEXT    	NOT NULL
+    ,street_number	TEXT		NOT NULL
+    ,country		TEXT		NOT NULL
+);
+
+CREATE TABLE users (
+    id         	        CHAR(36)        PRIMARY KEY
+    ,first_name         TEXT    		NOT NULL
+    ,last_name   		TEXT     		NOT NULL
+    ,billing_address_id CHAR(36)		NOT NULL REFERENCES addresses(id)
+    ,address_id 		CHAR(36)		NOT NULL REFERENCES addresses(id)
+    ,username   		TEXT     		NOT NULL UNIQUE
+    ,email      		TEXT    		NOT NULL
+    ,password   		TEXT  			NOT NULL
+);
+
+CREATE TABLE product_categories (
+    id      CHAR(36)    PRIMARY KEY
+    ,name   TEXT        NOT NULL
+);
+
+CREATE TABLE products (
+    id              CHAR(36)        PRIMARY KEY
+    ,name           TEXT    		NOT NULL
+    ,category_id    CHAR(36)    	NOT NULL REFERENCES product_categories(id)
+    ,price		    NUMERIC(2)	    NOT NULL 			
+);
+
+CREATE TABLE product_images (
+    id          	CHAR(36)        PRIMARY KEY
+    ,description	TEXT			NOT NULL
+    ,file_name      TEXT            NOT NULL
+    ,product_id 	CHAR(36)		NOT NULL REFERENCES products(id)
+);
+
+CREATE TABLE orders (
+    id          	CHAR(36)        PRIMARY KEY
+    ,total_price	NUMERIC(2)		NOT NULL
+    ,user_id        CHAR(36)        NOT NULL REFERENCES users(id)
+);
+
+CREATE TABLE orders_products (
+    order_id        CHAR(36)        NOT NULL REFERENCES orders(id)
+    ,product_id     CHAR(36)        NOT NULL REFERENCES products(id)
+    ,amount         INT             NOT NULL
+    ,PRIMARY KEY (order_id, product_id)
+);
+
+CREATE TABLE wishlist_products (
+    product_id      CHAR(36)        NOT NULL REFERENCES products(id)
+    ,user_id        CHAR(36)        NOT NULL REFERENCES users(id)
+    ,PRIMARY KEY(product_id, user_id)
+);
