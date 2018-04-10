531672c10d3043657756264dd262641046fe5841
Maximilian Narr <maxe.nr@live.de>
2018-04-10 23:22:20 +0200

    Updated DB Schema: Char(36) --> UUID

--- Doc/Schema.sql
+++ Doc/Schema.sql
@@ -8,7 +8,7 @@ DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS addresses;
 
 CREATE TABLE addresses (
-    id          	CHAR(36)    PRIMARY KEY
+    id          	UUID        PRIMARY KEY
     ,name	        TEXT	    NOT NULL
     ,street     	TEXT    	NOT NULL
     ,street_number	TEXT		NOT NULL
@@ -16,50 +16,50 @@ CREATE TABLE addresses (
 );
 
 CREATE TABLE users (
-    id         	        CHAR(36)        PRIMARY KEY
+    id         	        UUID            PRIMARY KEY
     ,first_name         TEXT    		NOT NULL
     ,last_name   		TEXT     		NOT NULL
-    ,billing_address_id CHAR(36)		NOT NULL REFERENCES addresses(id)
-    ,address_id 		CHAR(36)		NOT NULL REFERENCES addresses(id)
+    ,billing_address_id UUID		    NOT NULL REFERENCES addresses(id)
+    ,address_id 		UUID    		NOT NULL REFERENCES addresses(id)
     ,username   		TEXT     		NOT NULL UNIQUE
     ,email      		TEXT    		NOT NULL
     ,password   		TEXT  			NOT NULL
 );
 
 CREATE TABLE product_categories (
-    id      CHAR(36)    PRIMARY KEY
+    id      UUID        PRIMARY KEY
     ,name   TEXT        NOT NULL
 );
 
 CREATE TABLE products (
-    id              CHAR(36)        PRIMARY KEY
+    id              UUID        PRIMARY KEY
     ,name           TEXT    		NOT NULL
-    ,category_id    CHAR(36)    	NOT NULL REFERENCES product_categories(id)
+    ,category_id    UUID    	    NOT NULL REFERENCES product_categories(id)
     ,price		    NUMERIC(2)	    NOT NULL 			
 );
 
 CREATE TABLE product_images (
-    id          	CHAR(36)        PRIMARY KEY
+    id          	UUID            PRIMARY KEY
     ,description	TEXT			NOT NULL
     ,file_name      TEXT            NOT NULL
-    ,product_id 	CHAR(36)		NOT NULL REFERENCES products(id)
+    ,product_id 	UUID		    NOT NULL REFERENCES products(id)
 );
 
 CREATE TABLE orders (
-    id          	CHAR(36)        PRIMARY KEY
+    id          	UUID            PRIMARY KEY
     ,total_price	NUMERIC(2)		NOT NULL
-    ,user_id        CHAR(36)        NOT NULL REFERENCES users(id)
+    ,user_id        UUID            NOT NULL REFERENCES users(id)
 );
 
 CREATE TABLE orders_products (
-    order_id        CHAR(36)        NOT NULL REFERENCES orders(id)
-    ,product_id     CHAR(36)        NOT NULL REFERENCES products(id)
+    order_id        UUID            NOT NULL REFERENCES orders(id)
+    ,product_id     UUID            NOT NULL REFERENCES products(id)
     ,amount         INT             NOT NULL
     ,PRIMARY KEY (order_id, product_id)
 );
 
 CREATE TABLE wishlist_products (
-    product_id      CHAR(36)        NOT NULL REFERENCES products(id)
-    ,user_id        CHAR(36)        NOT NULL REFERENCES users(id)
+    product_id      UUID        NOT NULL REFERENCES products(id)
+    ,user_id        UUID        NOT NULL REFERENCES users(id)
     ,PRIMARY KEY(product_id, user_id)
 );
