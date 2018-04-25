4916a784c1ae93a0e145772c228dfdd11c4575d1
Daniel Rotärmel <ezekills@gmx.de>
2018-04-25 18:53:31 +0200

    added font-awesome

--- ClientApp/src/app/login/login.component.ts
+++ ClientApp/src/app/login/login.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { AppModule } from '../app.module';
 
 @Component({
-  selector: 'login-home',
+  selector: 'app-login-home',
   templateUrl: './login.component.html',
 })
 export class LoginComponent {
 
     public login = () =>{
-      
+
       //this.router.navigate(['home']);
     }
 }
