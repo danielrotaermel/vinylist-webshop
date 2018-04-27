548d9d80a33b6c227608c8bc556bac74928aa0b8
Daniel Rotärmel <ezekills@gmx.de>
2018-04-27 10:06:28 +0200

    converted to scss

--- ClientApp/src/app/navigation-bar/navigation-bar.component.ts
+++ ClientApp/src/app/navigation-bar/navigation-bar.component.ts
@@ -7,7 +7,7 @@ import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-navigation-bar',
   templateUrl: './navigation-bar.component.html',
-  styleUrls: ['./navigation-bar.component.css']
+  styleUrls: ['./navigation-bar.component.scss']
 })
 export class NavigationBarComponent implements OnInit {
   constructor(private translate: TranslateService) { }
