f9da7ceb8a129eef9267eedc64a244d052c44c6d
jw110 <jw110@hdm-stuttgart.de>
2018-05-05 11:53:37 +0200

    updated angular and fixed tests

--- ClientApp/src/app/footer/footer.component.spec.ts
+++ ClientApp/src/app/footer/footer.component.spec.ts
@@ -1,14 +1,32 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MockComponent } from '../test-helper/mock.component';
 import { FooterComponent } from './footer.component';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { TranslateService, TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import {Observable} from 'rxjs/Observable';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+let translations: any = {"TESTING": "Ich teste die Komponente"};
 
+class FakeLoader implements TranslateLoader {
+  getTranslation(lang: string): Observable<any> {
+    return Observable.of(translations);
+  }
+}
 describe('FooterComponent', () => {
   let component: FooterComponent;
   let fixture: ComponentFixture<FooterComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FooterComponent ]
+      declarations: [ FooterComponent,
+        MockComponent({ selector: 'app-language-switcher' }) ],
+        imports: [
+          AngularSvgIconModule,
+          TranslateModule.forRoot({
+            loader: {provide: TranslateLoader, useClass: FakeLoader}
+          }),
+          HttpClientModule,
+        ]
     })
     .compileComponents();
   }));
