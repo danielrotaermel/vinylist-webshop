8ac4cac7a17815c4161a25a715c0f7fc32758c51
jw110 <jw110@hdm-stuttgart.de>
2018-05-16 22:29:54 +0200

    Add styling, add logging, fix minor things

--- ClientApp/src/app/user/login/login.component.ts
+++ ClientApp/src/app/user/login/login.component.ts
@@ -1,41 +1,46 @@
-import { Component, Input } from '@angular/core';
-import { LoginService } from './login.service';
+import { Component, Input } from "@angular/core";
+import { LoginService } from "./login.service";
 import { TranslateService } from "@ngx-translate/core";
 import { Router } from "@angular/router";
 
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSnackBar } from "@angular/material/snack-bar";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
   providers: [LoginService]
 })
 
 /**
- * @author Alexander Merker
+ * @author Alexander Merker, Janina Wachendorfer
  */
-export class LoginComponent{
-  @Input() name: string;
-  private isVisible = false;
+export class LoginComponent {
+  email: string;
+  password: string;
 
-  constructor(private snackBar : MatSnackBar, private loginService : LoginService, private router : Router) {
+  constructor(
+    private snackBar: MatSnackBar,
+    private loginService: LoginService,
+    private router: Router
+  ) {}
 
+  openSnackBar(message, time) {
+    this.snackBar.open(message, "", {
+      duration: time
+    });
   }
 
-  openSnackBar() {
-    this.snackBar.open("Logged in successfully");
-    // this.snackBar.open("Logged in successfully", '', {
-    //   duration: 1500
-    // });
-  }
-
-  //TODO: LOG ERRORS
-  OnSubmit(email,password){
-     this.loginService.signin(email,password).subscribe((data : any)=>{
-     localStorage.setItem('userToken',data.access_token);
-     this.router.navigate(['/']);
-     this.openSnackBar();
-   });
+  performLogin() {
+    this.loginService.signin(this.email, this.password).subscribe(
+      (data: any) => {
+        localStorage.setItem("userToken", data.access_token);
+        this.router.navigate(["/"]);
+        this.openSnackBar("Login successfully", 1500);
+      },
+      (error: any) => {
+        this.openSnackBar(error, 5000);
+      }
+    );
   }
 }
