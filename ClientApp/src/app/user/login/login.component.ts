a199f8670985161b052643e25f0ff17f4d320f6d
Daniel Rotärmel <dr053@hdm-stuttgart.de>
2018-06-11 00:41:17 +0200

    Revert "Merge branch 'product_stuff' into 'master'"
    This reverts merge request !22

--- ClientApp/src/app/user/login/login.component.ts
+++ ClientApp/src/app/user/login/login.component.ts
@@ -1,18 +1,20 @@
-import { Component, Input, ElementRef } from "@angular/core";
-import { LoginService } from "./login.service";
-import { TranslateService } from "@ngx-translate/core";
-import { Router } from "@angular/router";
+import { Component, Input, ElementRef } from '@angular/core';
+import { LoginService } from './login.service';
+import { TranslateService } from '@ngx-translate/core';
+import { Router } from '@angular/router';
 
-import { MatSnackBar } from "@angular/material/snack-bar";
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+  providers: [LoginService]
+})
 
 /**
  * @author Alexander Merker, Janina Wachendorfer
  */
-@Component({
-  selector: "app-login",
-  templateUrl: "./login.component.html",
-  styleUrls: ["./login.component.scss"]
-})
 export class LoginComponent {
   email: string;
   password: string;
@@ -26,7 +28,7 @@ export class LoginComponent {
   ) {}
 
   openSnackBar(message, time) {
-    this.snackBar.open(message, "", {
+    this.snackBar.open(message, '', {
       duration: time
     });
   }
@@ -35,12 +37,12 @@ export class LoginComponent {
     this.loginService.signin(this.email, this.password).subscribe(
       (data: any) => {
         this.popover.close();
-        localStorage.setItem("userToken", data.access_token);
-        this.router.navigate(["/"]);
-        this.openSnackBar("Login successful", 1500);
+        localStorage.setItem('userToken', data.access_token);
+        this.router.navigate(['/']);
+        this.openSnackBar('Login successful', 1500);
       },
       (error: any) => {
-        this.openSnackBar("Wrong username or password", 5000);
+        this.openSnackBar('Wrong username or password', 5000);
       }
     );
   }
