561c86ea613f69cdbbb7858978c3af6a8dad2f93
jw110 <jw110@hdm-stuttgart.de>
2018-06-26 22:59:04 +0200

    Fixed and added tests

--- Webspec3/ClientApp/src/app/services/storage.service.spec.ts
+++ Webspec3/ClientApp/src/app/services/storage.service.spec.ts
@@ -1,15 +1,29 @@
 import { TestBed, inject } from '@angular/core/testing';
 
-import { StorageService } from './storage.service';
+import { StorageService, LocalStorageService } from './storage.service';
 
 describe('StorageService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [StorageService]
+      providers: [LocalStorageService]
     });
   });
 
-  it('should be created', inject([StorageService], (service: StorageService) => {
+  it('should be created', inject([LocalStorageService], (service: LocalStorageService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should store a given key value pair and load it again', () => {
+    let service = new LocalStorageService();
+    let key = 'key';
+    let value = {
+      test: 'value'
+    };
+
+    service.setItem(key, value);
+
+    let returned = service.getItem(key);
+    expect(returned).toBeDefined();
+    expect(returned.test).toBe(value.test);
+  });
 });
